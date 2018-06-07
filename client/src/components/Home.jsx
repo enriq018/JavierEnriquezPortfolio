4eb62ffd749d598513461f0af5afdadf0a3f0222
javi <enriq018coding@gmail.com>
2018-06-07 13:16:43 -0700

    add home resume

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import Resume from './Resume.jsx';
 
-const Home = props => (
+
+const Home = ({ resumeClick, resumeActive }) => (
   <div className="home">
     <div className="hero-body">
+    <Resume resumeClick={resumeClick} resumeActive={resumeActive}/>
       <div className="container has-text-centered">
         <div className="columns is-vcentered">
           <div className="column is-5">
@@ -11,27 +14,38 @@ const Home = props => (
             </figure>
           </div>
           <div className="column is-6 is-offset-1">
-            <h1 className="title is-2">
+            <h1 className="title is-1">
               Javier Enriquez
             </h1>
-            <h2 className="subtitle is-4">
+            <h2 className="subtitle is-3">
               Software Engineer
             </h2>
             <br/>
             <div className='buttonRow'>
               <p className="has-text-centered">
-                <a className="button is-info is-large is-outlined b">
-                  Resume
+                <a className="button is-info is-large is-outlined b" onClick={()=>resumeClick()}>
+                <span>Resume </span>
+                <span className="icon is-small">
+                <i className="far fa-address-card" aria-hidden="true" />
+              </span>
                 </a>
               </p>
               <p className="has-text-centered">
-                <a className="button is-info is-large is-outlined b">
-                  Linkedin
+                <a onClick={()=> window.open('https://www.linkedin.com/in/javier-enriquez/', '_blank')}
+                 className="button is-info is-large is-outlined b">
+                  <span>Linkedin</span>
+                  <span className="icon is-small">
+                  <i className="fab fa-linkedin" aria-hidden="true" />
+                </span>
                 </a>
               </p>
               <p className="has-text-centered">
-                <a className="button is-info is-large is-outlined b">
-                  Github
+                <a  onClick={()=> window.open('https://github.com/enriq018/', '_blank')}
+                 className="button is-info is-large is-outlined b">
+                 <span>GitHub </span>
+                 <span className="icon ">
+                   <i className="fab fa-github "></i>
+                 </span>
                 </a>
               </p>
             </div>
--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -3,13 +3,23 @@ import NavBar from './NavBar.jsx';
 import Home from './Home.jsx';
 import Projects from './Projects.jsx';
 
+
 class MainContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       view: 'Home',
+      resumeActive: false,
     };
     this.changeView = this.changeView.bind(this);
+    this.resumeClick = this.resumeClick.bind(this);
+  }
+
+  resumeClick() {
+    this.setState(
+      (prevState, props)=> {
+        return { resumeActive: !prevState.resumeActive };
+      });
   }
 
   changeView(view) {
@@ -18,7 +28,7 @@ class MainContainer extends React.Component {
 
   currentView() {
     const view = {
-      Home: <Home />,
+      Home: <Home resumeClick={this.resumeClick} resumeActive={this.state.resumeActive} />,
       Projects: <Projects />,
     };
     return view[this.state.view];
@@ -27,7 +37,7 @@ class MainContainer extends React.Component {
   render () {
     return (
       <section>
-        <NavBar changeView={this.changeView}/>
+        <NavBar changeView={this.changeView} />
         <div className="">
         {this.currentView()}
         </div>
